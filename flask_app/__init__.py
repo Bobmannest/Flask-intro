f00f2f1ec52be6fcc2d0aa7e17cab4577151ba41
Kalin K <kalin.kuzev@city.ac.uk>
2025-07-06 12:20:12 +0100

    fixed calling custom click commands in powershell
    Due to how my custom click commands were instantiated inside of the application factory, flask could not locate the custom click commands without further instruction on which function the custom commands were created in.

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,13 +1,15 @@
 import os
 from flask import Flask
 
+
 #Use 'flask --app flask_app:instantiate_app run --debug' to run in the Terminal
+#Use 'flask --app flask_app:instantiate_app init-db' to initialise database
 #go to http://127.0.0.1:5000/hello to see output
-def instantiate_app(test_config = None):
+def instantiate_app(test_config=None):
     app = Flask(__name__, instance_relative_config = True)
     app.config.from_mapping(
         SECRET_KEY='epic',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'flask_app.sqlite'),
     )
 
     if test_config is None:
@@ -24,7 +26,11 @@ def instantiate_app(test_config = None):
         pass
 
     @app.route('/hello')
-    def hello():  # put application's code here
+    def hello():  #App code
         return 'Hello World!'
+
+    #You can use '.' instead of 'flask_app' to import from root directory!
+    from . import db_init
+    db_init.init_app(app)
 
     return app
--- flask_app/db_init.py
+++ flask_app/db_init.py
@@ -1,5 +1,5 @@
-import sqlite3
-
+import sqlite3, click
+from datetime import datetime
 from flask import g, current_app
 
 
@@ -10,15 +10,43 @@ def db_connect():
         g.db = sqlite3.connect(
             #current points to app that handles request
             current_app.config['DATABASE'],
-            detect_types = sqlite3.PARSE_DECLTYPES
+            detect_types=sqlite3.PARSE_DECLTYPES
         )
         #Row tells connection to return rows
         g.db.row_factory = sqlite3.Row
     return g.db
 
-#If connection exists, it is closed
-def close_db(e=None):
-    db = g.pop('db', None)
 
+#If connection exists, it is closed
+def close_db_connection(e=None):
+    db = g.pop('db', None)
     if db is not None:
         db.close()
+
+
+#Initialises the database and also open the SQL file
+def init_db():
+    db = db_connect()
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf-8'))
+
+#Initialises app by closing database connection and adding the init-db command
+def init_app(app):
+    #Calls this when cleaning up after response has been returned
+    app.teardown_appcontext(close_db_connection)
+    #Adds new cmd that can be called with flask
+    app.cli.add_command(init_db_cmd)
+
+
+#Click is for quick setup via a custom command line command
+@click.command('init-db')
+def init_db_cmd():
+    init_db()
+    click.echo('Database init SUCCESS')
+
+#Converts timestamp type items into datetime(I think)
+sqlite3.register_converter(
+    'timestamp', lambda v: datetime.fromisoformat(v.decode())
+)
+
+
